4146de9abd0e46be63a1d12d9c572839e11b54ac
gganwoor <139461535+gganwoor@users.noreply.github.com>
2026-05-19 06:29:24 +0900

    trivial edit

---
+++ Assets/Scripts/LetterboxScaler.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class LetterboxScaler : MonoBehaviour
+{
+    public float targetAspect = 16f / 9f;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        ApplyLetterbox();
+    }
+
+    void ApplyLetterbox()
+    {
+        float windowAspect = (float)Screen.width / (float)Screen.height;
+        float scaleHeight = windowAspect / targetAspect;
+
+        Camera cam = Camera.main;
+
+        if (scaleHeight < 1f)
+        {
+            Rect rect = cam.rect;
+            rect.width = 1f;
+            rect.height = scaleHeight;
+            rect.x = 0f;
+            rect.y = (1f - scaleHeight) / 2f;
+            cam.rect = rect;
+        }
+        else
+        {
+            float scaleWidth = 1f / scaleHeight;
+            Rect rect = cam.rect;
+            rect.width = scaleWidth;
+            rect.height = 1f;
+            rect.x = (1f - scaleWidth) / 2f;
+            rect.y = 0f;
+            cam.rect = rect;
+        }
+    }
+}
